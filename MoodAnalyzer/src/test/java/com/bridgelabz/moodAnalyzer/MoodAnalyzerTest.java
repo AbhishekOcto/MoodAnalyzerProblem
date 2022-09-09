d55a683056e31e37f5b901a39de6d4c6ef81f522
Abhishek <you@example.com>
2022-09-09 20:42:01 +0530

    [Abhishek] Added:TC1.1 repeat Calling analyze mood method with no params

--- MoodAnalyzer/src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyzerTest {
     @Test
     public void messageIsSad_ReturnSad(){
-        MoodAnalyzer moodAnalyze = new MoodAnalyzer("This is Sad message");
+        MoodAnalyzer moodAnalyze = new MoodAnalyzer("I am in Sad Mood");
         String result1 = moodAnalyze.analyzeMood();
         Assertions.assertEquals("SAD",result1);
     }
-    @Test
-    public void messageIsNotSad_ReturnHappy(){
-        MoodAnalyzer moodAnalyze = new MoodAnalyzer("This is HAPPY message");
-        String result2 = moodAnalyze.analyzeMood();
-        Assertions.assertEquals("HAPPY",result2);
-    }
+//    @Test
+//    public void messageIsNotSad_ReturnHappy(){
+//        MoodAnalyzer moodAnalyze = new MoodAnalyzer("This is HAPPY message");
+//        String result2 = moodAnalyze.analyzeMood();
+//        Assertions.assertEquals("HAPPY",result2);
+//    }
 }
